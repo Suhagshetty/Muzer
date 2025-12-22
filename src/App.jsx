b827191fbfdac49b9490dd1f259ce75137d53137
Suhagshetty <suhagshetty07@gmail.com>
2025-12-22 13:06:00 +0530

    added-homeDisplay-feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import React from "react";
+import Display from "./component/Display";
 import Player from "./component/Player";
 import Sidebar from "./component/Sidebar";
-function App() {
+const App = () => {
   return (
-    <div>
-      <div>
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
         <Sidebar />
+        <Display />
       </div>
       <Player />
     </div>
   );
-}
+};
+
 export default App;
