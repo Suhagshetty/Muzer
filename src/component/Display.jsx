3cc4fe19538de8aaca882077742b26ddca84081e
Suhagshetty <suhagshetty07@gmail.com>
2025-12-22 23:13:01 +0530

    removed #1 dd and added theme

--- src/component/Display.jsx
+++ src/component/Display.jsx
@@ -1,10 +1,29 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import DisplayHome from "./DisplayHome";
+import { useRef } from "react";
 import DisplayAlbum from "./DisplayAlbum";
+import { albumsData } from "../assets/assets";
 const Display = () => {
+  const displayRef = useRef();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album");
+  console.log(isAlbum);
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  const bgColor = albumsData[Number(albumId)].bgColor;
+  console.log(bgColor);
+  useEffect(() => {
+    if (isAlbum) {
+      displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`;
+    } else {
+      displayRef.current.style.background = `#121212`;
+    }
+  });
   return (
-    <div className="w-full m-2 px-6 pt-4 rounded bg-[#212121] text-white overflow-auto lg:w-[75%] lg:ml-0">
+    <div
+      ref={displayRef}
+      className="w-full m-2 px-6 pt-4 rounded bg-[#212121] text-white overflow-auto lg:w-[75%] lg:ml-0"
+    >
       <Routes>
         <Route path="/" element={<DisplayHome />} />
         <Route path="/album/:id" element={<DisplayAlbum />} />
